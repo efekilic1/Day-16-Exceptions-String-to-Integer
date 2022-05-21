eb3329a9cb6f2472e7a84b60769c04e9f6aac73b
Muhammed KILIÇ <105243448+mhmmdklc@users.noreply.github.com>
2022-05-21 19:00:22 +0300

    Add files via upload

---
+++ try-catch/try-catch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace trycatch
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try{
+                Console.WriteLine(Convert.ToInt32(S));
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Bad String");
+            }
+            
+        }
+    }
+    
+}
